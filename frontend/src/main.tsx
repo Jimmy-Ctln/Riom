6ae20fab8f2562942bc9e189c006d956c42ce4b7
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-01-24 00:01:37 +0100

    feat(frontend): Update main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./routes/routes";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
